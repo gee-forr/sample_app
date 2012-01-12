ebe5d654974fff4cf04883949b99cfad8e86b94f
Gabriel Fortuna <gee.forr@gmail.com>
2012-01-12 16:06:12 +0200

    Added named routes for some pages, deleted public/index.html file as well

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ SampleApp::Application.routes.draw do
   get "pages/about"
   get "pages/help"
 
+  root              to: 'pages#home'
+  match '/contact', to: 'pages#contact'
+  match '/about',   to: 'pages#about'
+  match '/help',    to: 'pages#help'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
