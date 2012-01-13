0f841f1fad70d4850e5bea23413a3153cafd53f5
Gabriel Fortuna <gee.forr@gmail.com>
2012-01-13 09:46:09 +0200

    Rearranged routes to put named routes first

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 SampleApp::Application.routes.draw do
+  root              to: 'pages#home'
+  match '/signup',  to: 'users#new'
+  match '/contact', to: 'pages#contact'
+  match '/about',   to: 'pages#about'
+  match '/help',    to: 'pages#help'
+
   get "users/new"
 
   get "pages/home"
   get "pages/contact"
   get "pages/about"
   get "pages/help"
-
-  root              to: 'pages#home'
-  match '/signup',  to: 'users#new'
-  match '/contact', to: 'pages#contact'
-  match '/about',   to: 'pages#about'
-  match '/help',    to: 'pages#help'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
